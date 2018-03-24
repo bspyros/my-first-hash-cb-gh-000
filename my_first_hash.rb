0d418e18296a4b87d0e5bbdceda7e0f1e4feb100
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-24 10:45:10 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,7 +10,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" = 2, "oil paintings" = 3
-    
+
 end
 
 def retrieval
@@ -32,7 +32,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
-  
+
 
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
